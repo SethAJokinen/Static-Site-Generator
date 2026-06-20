aaae283c88b399bfa6adf9279272591848c529b2
Seth <sethajokinen@gmail.com>
2026-06-20 18:25:10 -0500

    Finally done!

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 import shutil
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
     
     with open(from_path) as content:
@@ -18,13 +18,14 @@ def generate_page(from_path, template_path, dest_path):
 
     html_title = template_content.replace(f"{{ Title }}", f"{title}")
     html_page = html_title.replace("{{ Content }}", f"{html_content}")
+    href_and_src_replaced_html_page = html_page.replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as content:
-        content.write(html_page)
+        content.write(href_and_src_replaced_html_page)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     content_dir = Path(dir_path_content)
     dest_dir = Path(dest_dir_path)
 
@@ -34,12 +35,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         corresponding_dest = dest_dir / relative_path
 
         if item.is_dir():
-            generate_pages_recursive(item, template_path, corresponding_dest)
+            generate_pages_recursive(item, template_path, corresponding_dest, base_path)
 
         elif item.is_file() and item.suffix == ".md":
             html_dest_path = corresponding_dest.with_suffix(".html")
 
-            generate_page(str(item), str(template_path), str(html_dest_path))
+            generate_page(str(item), str(template_path), str(html_dest_path), base_path)
 
         elif item.is_file():
             shutil.copy2(item, corresponding_dest)
--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from pathlib import Path
 import shutil, os
 from static_to_public import static_to_public
 from generate_page import generate_page, generate_pages_recursive
+import sys
 
 
 def main():
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+    else:
+        base_path = "/"
     public_path = Path("./public")
     if os.path.exists(public_path):
         for item in public_path.iterdir():
@@ -15,5 +20,5 @@ def main():
 
     static_path = Path("./static")
     static_to_public(static_path, public_path)
-    generate_pages_recursive("./content", "./template.html", "./public")
+    generate_pages_recursive("./content", "./template.html", "./docs", base_path)
 main()
